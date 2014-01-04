2b72d325b4f3c8b7928620fb7746f5c9bfb096b9
pitagoric <pitagoric@MacBook-Air-di-pitagoric.local>
2014-01-04 15:46:07 +0100

    Initial Commit

---
+++ sfere/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  sfere
+//
+//  Created by pitagoric on 04/01/14.
+//  Copyright (c) 2014 pitagoric. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
